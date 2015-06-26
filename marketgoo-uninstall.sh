21dd1491239048b32c082ac31c7be34dff37e18a
Jose Miguel Pérez Ruiz <josemiguel@perezruiz.com>
2015-06-26 17:42:15 +0200

    Fixed uninstall script to support removing dynamicui items.

--- marketgoo-uninstall.sh
+++ marketgoo-uninstall.sh
@@ -64,7 +64,10 @@ uninstall_whm_addon()
 uninstall_cpanel_plugin()
 {
     $MKTGOODIR/uninstall_plugins.sh
-    for i in $THEMES; do rm -rf $THEMEDIR/$i/marketgoo/ >/dev/null 2>&1; done
+    for i in $THEMES; do
+        rm -rf $THEMEDIR/$i/marketgoo/ >/dev/null 2>&1
+        rm -rf $THEMEDIR/$i/dynamicui/dynamicui_marketgoo* >/dev/null 2>&1
+    done
     rm -rf $MKTGOODIR
 }
 
